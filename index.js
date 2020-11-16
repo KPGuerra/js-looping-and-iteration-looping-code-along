df37e612275d4cc8a88cdaec5c31c89031dbaf8a
KPGuerra <KPGuerra96@gmail.com>
2020-11-15 23:58:58 -0500

    commit tree ac15dce58aa10859ad3fc8e99530c186be4c08c0

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Code your solutions in this file
+function writeCards(namesArray, event) {
+    let cards = []
+    for (let i = 0; i < namesArray.length; i++) {
+        cards.push( `Thank you, ${namesArray[i]}, for the wonderful ${event} gift!` )
+    }
+    return cards
+}
+
+
+
+function countDown(num) {
+    while (num > 0) {
+        console.log(num);
+        num -= 1;
+    }
+    console.log(0)
+}
+
+console.log(countDown);
+
+
